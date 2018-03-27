616efb9d5b55e8b06e01e6bed5ad97002cff7d9f
ilya <ilyaaa.1997.13@gmail.com>
2018-03-27 11:49:18 +0300

    added searching min distance between points

--- src/sample/PolygonCreator.java
+++ src/sample/PolygonCreator.java
@@ -1,5 +1,6 @@
 package sample;
 
+import javafx.geometry.Point2D;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.*;
 
@@ -7,14 +8,15 @@ import java.util.ArrayList;
 
 public class PolygonCreator {
     private Controller controller;
-    public ArrayList<Point> points = new ArrayList<>();
+
+    private Polygon polygon = new Polygon();
+    private ArrayList<Point> points = new ArrayList<>();
 
     public PolygonCreator(Controller controller){
         this.controller = controller;
     }
 
     public void createPolygon() {
-        Polygon polygon = new Polygon();
         polygon = setPoint(polygon, 24, 24);
         polygon = setPoint(polygon, 55, 0);
         polygon = setPoint(polygon, 112, 45);
@@ -37,22 +39,59 @@ public class PolygonCreator {
     }
 
     public void breakPolygon() {
+        for (int i = 0; i < points.size(); i++){
+            Point p0;
+            if (i == 0) p0 = points.get(points.size() - 1);
+            else p0 = points.get(i - 1);
+            Point p1 = points.get(i);
+            Point p2;
+            if (i == points.size() - 1) p2 = points.get(0);
+            else p2 = points.get(i + 1);
+
+            Point2D midPoint = p0.midpoint(p2);
+            double secondX = p1.getX();
+            double secondY = p1.getY();
+
+            int xCounter = 0;
+            if (secondX > midPoint.getX()) xCounter = -1;
+            else if (secondX < midPoint.getX()) xCounter = 1;
+
+            int yCounter = 0;
+            if (secondY > midPoint.getY()) yCounter = -1;
+            else if (secondY < midPoint.getY()) yCounter = 1;
+
+            double difference = Math.abs(midPoint.getX()) - secondX/Math.abs(midPoint.getY() - secondY);
+            double angle = 100/90 * 0.01 * midPoint.angle(secondX, secondY);
+            if (difference < 1) {
+                secondY += yCounter * (angle + 2);
+                secondX += xCounter * (1 - angle + 2);
+            } else if (difference > 1) {
+                secondY += yCounter * (1 - angle + 2);
+                secondX += xCounter * (angle + 2);
+            }
+            else {
+                secondX += xCounter * 2;
+                secondY += yCounter * 2;
+            }
+
+            Circle test = new Circle();
+                test.setRadius(1);
+                test.setCenterX(secondX);
+                test.setCenterY(secondY);
+                controller.pane.getChildren().add(test);
+            System.out.println(!polygon.contains(new Point2D(secondX, secondY)));
+        }
+
+
         Point point1 = points.get(points.size() - 1);
         Point point2 = points.get(0);
 
         for (int i = 1; i < points.size(); i++){
-            System.out.println(getDistance(point1, point2));
-            if (getDistance(point1, point2) < getDistance(point2, points.get(i))){
+            if (point1.distance(point2) > point2.distance(points.get(i))){
                 point1 = point2;
                 point2 = points.get(i);
             }
         }
-
-        System.out.println("max" + getDistance(point1, point2));
-    }
-
-    private double getDistance(Point point1, Point point2){
-        return Math.sqrt(Math.pow(point1.x - point2.x, 2) + Math.pow(point1.y - point2.y, 2));
     }
 
     private Polygon setPoint(Polygon polygon, double pointX, double pointY){
@@ -63,13 +102,15 @@ public class PolygonCreator {
         return polygon;
     }
 
-    private class Point {
-        public double x;
-        public double y;
+    private class Point extends Point2D {
+        private boolean isAngleCorrect;
 
         Point(double x, double y){
-            this.x = x;
-            this.y = y;
+            super(x, y);
         }
+
+        public boolean getAngleCorrect() { return isAngleCorrect; }
+
+        public void setAngleCorrect(boolean angle) { isAngleCorrect = angle; }
     }
 }
