2076ff0da416839336505ee4d156068c54c13be7
Brendibek <brendibek.k@yandex.ru>
2018-04-02 13:28:49 +0300

    polyrect2

--- src/sample/PolygonCreator.java
+++ src/sample/PolygonCreator.java
@@ -17,70 +17,26 @@ public class PolygonCreator {
     }
 
     public void createPolygon() {
-        polygon = setPoint(polygon, 24, 24);
-        polygon = setPoint(polygon, 55, 0);
-        polygon = setPoint(polygon, 112, 45);
-        polygon = setPoint(polygon, 112, 108);
-        polygon = setPoint(polygon, 189, 169);
-        polygon = setPoint(polygon, 226, 169);
-        polygon = setPoint(polygon, 226, 198);
-        polygon = setPoint(polygon, 172, 198);
-        polygon = setPoint(polygon, 106, 146);
-        polygon = setPoint(polygon, 40, 197);
-        polygon = setPoint(polygon, 40, 222);
-        polygon = setPoint(polygon, 0, 222);
-        polygon = setPoint(polygon, 0, 197);
-        polygon = setPoint(polygon, 58, 152);
-        polygon = setPoint(polygon, 58, 50);
+        polygon = setPointPolygon(polygon, 24, 24);
+        polygon = setPointPolygon(polygon, 55, 0);
+        polygon = setPointPolygon(polygon, 112, 45);
+        polygon = setPointPolygon(polygon, 112, 108);
+        polygon = setPointPolygon(polygon, 189, 169);
+        polygon = setPointPolygon(polygon, 226, 169);
+        polygon = setPointPolygon(polygon, 226, 198);
+        polygon = setPointPolygon(polygon, 172, 198);
+        polygon = setPointPolygon(polygon, 106, 146);
+        polygon = setPointPolygon(polygon, 40, 197);
+        polygon = setPointPolygon(polygon, 40, 222);
+        polygon = setPointPolygon(polygon, 0, 222);
+        polygon = setPointPolygon(polygon, 0, 197);
+        polygon = setPointPolygon(polygon, 58, 152);
+        polygon = setPointPolygon(polygon, 58, 50);
         polygon.setFill(Color.valueOf("#999999"));
         controller.pane.getChildren().add(polygon);
     }
 
     public void breakPolygon() {
-//        for (int i = 0; i < points.size(); i++){
-//            Point p0;
-//            if (i == 0) p0 = points.get(points.size() - 1);
-//            else p0 = points.get(i - 1);
-//            Point p1 = points.get(i);
-//            Point p2;
-//            if (i == points.size() - 1) p2 = points.get(0);
-//            else p2 = points.get(i + 1);
-//
-//            Point2D midPoint = p0.midpoint(p2);
-//            double secondX = p1.getX();
-//            double secondY = p1.getY();
-//
-//            int xCounter = 0;
-//            if (secondX > midPoint.getX()) xCounter = -1;
-//            else if (secondX < midPoint.getX()) xCounter = 1;
-//
-//            int yCounter = 0;
-//            if (secondY > midPoint.getY()) yCounter = -1;
-//            else if (secondY < midPoint.getY()) yCounter = 1;
-//
-//            double difference = Math.abs(midPoint.getX()) - secondX/Math.abs(midPoint.getY() - secondY);
-//            double angle = 100/90 * 0.01 * midPoint.angle(secondX, secondY);
-//            if (difference < 1) {
-//                secondY += yCounter * (angle + 2);
-//                secondX += xCounter * (1 - angle + 2);
-//            } else if (difference > 1) {
-//                secondY += yCounter * (1 - angle + 2);
-//                secondX += xCounter * (angle + 2);
-//            }
-//            else {
-//                secondX += xCounter * 2;
-//                secondY += yCounter * 2;
-//            }
-//
-//            Circle test = new Circle();
-//                test.setRadius(1);
-//                test.setCenterX(secondX);
-//                test.setCenterY(secondY);
-//                controller.pane.getChildren().add(test);
-//            System.out.println(!polygon.contains(new Point2D(secondX, secondY)));
-//        }
-//
-//
 //        Point point1 = points.get(points.size() - 1);
 //        Point point2 = points.get(0);
 //
@@ -112,30 +68,112 @@ public class PolygonCreator {
             double speedX = Math.abs(x2 - midPointX)/distance;
             double speedY = Math.abs(y2 - midPointY)/distance;
             double movePointX = midPointX, movePointY = midPointY;
-            boolean check = false;
-            for(int j = 1; j < distance; j+=1){
-                if(midPointX > x2) movePointX -= speedX;
-                else movePointX += speedX;
 
-                if(midPointY > y2) movePointY -= speedY;
-                else movePointY += speedY;
+            int distanceCounter;
+            if((int)distance == distance) distanceCounter = (int)distance - 1;
+            else distanceCounter = (int)distance;
+
+            if(midPointX > x2) movePointX -= speedX * distanceCounter;
+            else movePointX += speedX * distanceCounter;
+
+            if(midPointY > y2) movePointY -= speedY * distanceCounter;
+            else movePointY += speedY * distanceCounter;
+
+            points.get(num2).setAngleCorrect(!polygon.contains(movePointX, movePointY));
+        }
+        for(int numPoint = 0; numPoint < points.size(); numPoint++)
+        {
+            Point point = points.get(numPoint);
+
+            Point nextPoint;
+            if (numPoint == points.size() - 1) nextPoint = points.get(0);
+            else nextPoint = points.get(numPoint + 1);
+
+            if (!(point.getAngleCorrect() && !nextPoint.getAngleCorrect())) continue;
+
+            double vectorX = nextPoint.getX() - point.getX();
+            double vectorY = nextPoint.getY() - point.getY();
+
+            double c = -(vectorX * point.getX()) - (vectorY * point.getY());
+
+            Point2D p1, p2;
+            if(vectorX == 0){
+                p1 = new Point2D(point.getX() + 1, point.getY());
+                p2 = new Point2D(point.getX() - 1, point.getY());
+            }else if(vectorY == 0){
+                p1 = new Point2D(point.getX(), point.getY() + 1);
+                p2 = new Point2D(point.getX(), point.getY() - 1);
+            }else{
+                p1 = new Point2D(point.getX() + 1, ((-vectorX * (point.getX() + 1)) - c) / vectorY);
+                p2 = new Point2D(point.getX() - 1, ((-vectorX * (point.getX() - 1)) - c) / vectorY);
+            }
+
+            int counter;
+            double x, y;
+            if (polygon.contains(p1)) {
+                counter = 1;
+                x = p1.getX();
+                y = p1.getY();
+            }
+            else if (polygon.contains(p2)) {
+                counter = -1;
+                x = p2.getX();
+                y = p2.getY();
+            }
+            else {
+                System.out.println("Error");
+                break;
+            }
+            while (polygon.contains(x, y)) {
+                if(vectorX == 0) x += counter;
+                else if(vectorY == 0) y += counter;
+                else{
+                    x += counter;
+                    y = ((-vectorX * x) - c) / vectorY;
+                }
 
                 Circle test = new Circle();
                 test.setRadius(1);
-                test.setCenterX(movePointX);
-                test.setCenterY(movePointY);
+                test.setCenterX(x);
+                test.setCenterY(y);
+                test.setFill(Color.valueOf("#F41219"));
                 controller.pane.getChildren().add(test);
-                if(!polygon.contains(movePointX, movePointY)) check = true;
-                else check = false;
             }
+            createRectFromThreePoints(nextPoint.getX(), nextPoint.getY(), point.getX(), point.getY(), x, y);
+            point.setAngleCorrect(false);
 
-            points.get(num1).setAngleCorrect(check);
+            boolean checkTruePoint = false;
+            for (int i = 0; i < points.size(); i++){
+                if(points.get(i).getAngleCorrect()) checkTruePoint = true;
+            }
+            if ((numPoint == points.size() - 1)&&(checkTruePoint)) numPoint = 0;
         }
+        System.out.println("Done");
     }
 
-    private Polygon setPoint(Polygon polygon, double pointX, double pointY){
+    private void createRectFromThreePoints(double x1, double y1, double x2, double y2, double x3, double y3){
+        Polygon polyRect = new Polygon();
+        double x4 = x1 + x3 - x2;
+        double y4 = y1 + y3 - y2;
+        polyRect = setPointRect(polyRect, x1, y1);
+        polyRect = setPointRect(polyRect, x2, y2);
+        polyRect = setPointRect(polyRect, x3, y3);
+        polyRect = setPointRect(polyRect, x4, y4);
+
+        System.out.println(x1 + " " + y1 + "|" + x2 + " " + y2 + "|" + x3 + " " + y3 + "|" + x4 + " " + y4);
+        polyRect.setFill(Color.valueOf("#333333"));
+        controller.pane.getChildren().add(polyRect);
+    }
+
+    private Polygon setPointPolygon(Polygon polygon, double pointX, double pointY){
         points.add(new Point(pointX, pointY));
 
+        polygon.getPoints().add(pointX);
+        polygon.getPoints().add(pointY);
+        return polygon;
+    }
+
+    private Polygon setPointRect(Polygon polygon, double pointX, double pointY){
         polygon.getPoints().add(pointX);
         polygon.getPoints().add(pointY);
         return polygon;
